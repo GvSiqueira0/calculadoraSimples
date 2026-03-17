fa110625c2611f1a45420ac918e8a81602b9cba8
GvSiqueira0 <gabrielcvo894@gmail.com>
2026-03-17 19:15:03 -0300

    Add OperacoesBasicas with soma/subtracao
    Introduce OperacoesBasicas.java providing two static methods: soma(double a, double b) and subtracao(double a, double b) to perform basic addition and subtraction. Adds a simple utility class under src for reuse.

---
+++ src/OperacoesBasicas.java
@@ -0,0 +1,10 @@
+public class OperacoesBasicas {
+
+    public static double soma(double a, double b) {
+        return a + b;
+    }
+
+    public static double subtracao(double a, double b) {
+        return a - b;
+    }
+}
