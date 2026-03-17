ddc068ddaeb805b32e3bab0f6904dc6d246830e6
PedroRufino-03 <Pedro_SH007@hotmail.com>
2026-03-17 19:28:23 -0300

    Create OperacoesAvancadas.java

---
+++ src/OperacoesAvancadas.java
@@ -0,0 +1,16 @@
+public class OperacoesAvancadas {
+
+    public static double multiplicacao(double a, double b) {
+        return a * b;
+    }
+
+    public static double divisao(double a, double b) {
+
+        if (b == 0) {
+            System.out.println("Erro: divisão por zero!");
+            return 0;
+        }
+
+        return a / b;
+    }
+}
